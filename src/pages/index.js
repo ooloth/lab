9bc002efd3acb10e5b03e676995d58afd33c016b
Michael Uloth <hello@michaeluloth.com>
2019-03-12 15:10:52 -0400

    📦NEW: add my poker range reference component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 const LabPage = () => (
   <Base>
-    {/* <PageMetadata page={data.metadata.siteMetadata.labPage} /> */}
-
     <main className="avenir" style={{ minHeight: `calc(100vh - 36px)` }}>
       <LabHero title="Lab" description="Experiments with app-building." />
 
@@ -14,23 +12,18 @@ const LabPage = () => (
 
 // TODO: add some kind of image/icon to go with each link (turn links into squares with an image/icon, title, and maybe a one-line description)
 const PageLinks = () => (
-  <section className="container pv5">
-    <ul className="flex flex-wrap nl4 nt4">
+  <section>
+    <ul>
       {labLinks.map((link, i) => (
-        <li key={i} className="pl4 pt4 w-100 sm:w-50 md:w-third">
-          <Link
-            to={`/${link}/`}
-            className="db shadow-lg bg-green pa3 tc f4 fw9 ttc animate hover:bg-black hover:white"
-          >
-            {link}
-          </Link>
+        <li key={i}>
+          <Link to={`/${link}/`}>{link}</Link>
         </li>
       ))}
     </ul>
   </section>
 )
 
-const labLinks = [`cards`, `mines`, `moves`, `news`, `nines`, `odds`]
+const labLinks = [`cards`, `mines`, `moves`, `news`, `nines`, `poker`, `odds`]
 
 /*
  *
